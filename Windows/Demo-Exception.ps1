bd93ff62ae0d706638c02094213f397fa66f9d5f
CMarshall44 <95226233+CMarshall44@users.noreply.github.com>
2025-11-21 20:55:22 +0000

    Create Demo-Exception.ps1

---
+++ Windows/Demo-Exception.ps1
@@ -0,0 +1,20 @@
+﻿function Test-Exception{
+    Try{
+        $path = "C:\ThisDoesn't\Exist.txt"
+        Remove-Item -Path $path -ErrorAction Stop
+    }
+    catch [System.Management.Automation.ItemNotFoundException]{
+        Write-Error -Exception ([System.IO.FileNotFoundException]::new("Could not find path: $path", $_.Exception)) -ErrorAction Stop
+        #throw [System.IO.FileNotFoundException]::new("Could not find path: $Path", $_.Exception)
+    }
+    catch{
+        #$_.Exception
+    }
+}
+
+try{
+    Test-Exception
+}
+catch{
+    Write-Host $_.Exception
+}
